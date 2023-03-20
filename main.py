cbed7fdaa689d5cc2add4877a7d2d0feaafb6dc6
unknown <henrique.ph935@gmail.com>
2023-03-20 13:48:16 -0300

    Cria funcao de listar um elemento específico no vetor

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ menu = int(input("Digite a opção desejada: "))
 if menu == 1:
     vetor_teste = vetor.Vetor(2)
     vetor_teste.inserir_elemento_posicao(1, 0)
+
+    print(vetor_teste.listar_elemento(0))
--- vetores/vetor.py
+++ vetores/vetor.py
@@ -6,3 +6,7 @@ class Vetor():
 
     def inserir_elemento_posicao(self, elemento, posicao):
         self.__elementos[posicao] = elemento
+
+
+    def listar_elemento(self, posicao):
+        return self.__elementos[posicao]
