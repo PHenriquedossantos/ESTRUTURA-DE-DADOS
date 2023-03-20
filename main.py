beeebc5c7d5798eb9fdf8529d396835af2f85835
unknown <henrique.ph935@gmail.com>
2023-03-20 13:46:55 -0300

    Cria funcao de inserir elemento em uma posicao do vetor

---
+++ main.py
@@ -0,0 +1,11 @@
+from vetores import vetor
+
+print(30 * "-", "MENU", 30*"-")
+print("1. Vetores")
+print("2. Listas Ligadas")
+
+menu = int(input("Digite a opção desejada: "))
+
+if menu == 1:
+    vetor_teste = vetor.Vetor(2)
+    vetor_teste.inserir_elemento_posicao(1, 0)
--- vetores/vetor.py
+++ vetores/vetor.py
@@ -1,5 +1,8 @@
 class Vetor():
     def __init__(self, tamanho):
         self.__tamanho = tamanho
-        self.__elementos = tamanho * [None]
+        self.__elementos = tamanho * [None] # [None, None, None...]
         self.__posicao = 0
+
+    def inserir_elemento_posicao(self, elemento, posicao):
+        self.__elementos[posicao] = elemento
