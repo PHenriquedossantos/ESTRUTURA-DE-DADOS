524bbf349531cda58475392f52109bba59772d06
unknown <henrique.ph935@gmail.com>
2023-03-22 22:30:32 -0300

    Cria funcao push

--- pilha/stack.py
+++ pilha/stack.py
@@ -1,11 +1,26 @@
 from node import Node
 
+# inserir na pilha
+# remover da pilha
+# observar o topo da pilha
+
 class Stack:
     def __init__(self):
-        self.head = None
+        self.top = None
         self._size = 0
 
+    def push(self, elem):
+        # insere um elemento na pilha
+        node = Node(elem)
 
+        self.top = node
+        node.next = self.top
+    def pop(self):
+        # Remove o elemento do topo da pilha
+        ...
+    def peek(self):
+        # retorna o topo da pilha sem remover
+        ...
     def __len__(self):
         """Retorna o tamanho da lista"""
         return self._size
