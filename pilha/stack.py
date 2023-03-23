e1a25829021314f6bf26b9112c51d9950ce84bdd
unknown <henrique.ph935@gmail.com>
2023-03-22 22:33:49 -0300

    Cria funcao pop e peek

--- pilha/stack.py
+++ pilha/stack.py
@@ -12,15 +12,22 @@ class Stack:
     def push(self, elem):
         # insere um elemento na pilha
         node = Node(elem)
-
-        self.top = node
         node.next = self.top
+        self.top = node
+        self._size +=1
     def pop(self):
         # Remove o elemento do topo da pilha
-        ...
+        if self._size > 0:
+            node = self.top
+            self.top = self.top.next
+            self._size -=1
+            return node
+        raise IndexError("The stack is empty")
+    
     def peek(self):
-        # retorna o topo da pilha sem remover
-        ...
+        if self._size > 0:
+            return self.top
+        raise IndexError("The stack is empty")
     def __len__(self):
         """Retorna o tamanho da lista"""
         return self._size
